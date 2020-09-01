adcf72035cd49f47131a8ccb093a6eabf3ab8136
John Hurring Jr <jhurring@google.com>
2020-09-01 09:57:05 -0400

    Testing out temporary access with credentials

---
+++ test/conditions/temporary-project/main.tf
@@ -0,0 +1,28 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/******************************************
+  Policy Data
+ *****************************************/
+
+resource "google_project_iam_member" "temporary_break_glass_project_iam_member" {
+  member  = "user:${var.user}"
+  role    = var.role
+
+  condition {
+    expression = "request.time < timestamp (\"+ ${var.duration}\")"
+    title = "temporary-break-glass-${var.project_id}"
+  }
+}
