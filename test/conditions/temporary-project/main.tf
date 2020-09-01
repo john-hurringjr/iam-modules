77022dfee1056ad038ccc41c4f8db985db6d0f7d
John Hurring Jr <jhurring@google.com>
2020-09-01 16:27:00 -0400

    Updating to add in timestamp ref

--- test/conditions/temporary-project/main.tf
+++ test/conditions/temporary-project/main.tf
@@ -23,7 +23,7 @@ resource "google_project_iam_member" "temporary_break_glass_project_iam_member" 
   role    = var.role
 
   condition {
-    expression = "request.time < timestamp (\"${timestamp()}\")"
+    expression = "request.time < timestamp (\"${timeadd(timestamp(), 3600s)}\")"
     title = "temporary-break-glass-${var.project_id}"
   }
 }
